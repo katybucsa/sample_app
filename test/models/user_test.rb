a8ac6349e7b6d4ce30d2fb31ddf3c1316735a0df
Ecaterina Bucsa <katybucsa@yahoo.com>
2021-02-13 19:51:15 +0000

    Implement basic login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
  
  def setup
-   @user = User.new(name: "Example User", email: "user@example.com",
+   @user = User.new(name: "Example User", email: "user1@example.com",
                     password: "foobar", password_confirmation: "foobar")
  end
  
@@ -44,7 +44,7 @@ class UserTest < ActiveSupport::TestCase
  
  test "email validation should accept valid addresses" do
   
-  valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+  valid_addresses = %w[user1@example.com USER@foo.COM A_US-ER@foo.bar.org
   first.last@foo.jp alice+bob@baz.cn]
   valid_addresses.each do |valid_address|
    @user.email = valid_address
